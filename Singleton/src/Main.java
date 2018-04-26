5d514cd0e9ffe4d36d2eb6773255b34db648cbc1
Lucas S <lucas.soaresouza@gmail.com>
2018-04-26 15:40:38 -0300

    Adding hash of cavaleiros on print

--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		
 		System.out.println("\n=========================Descricao dos cavaleiros criados==========================\n");
 		System.out.println("Cavaleiro " + cavaleiro1.retornaNome() + " de level " + cavaleiro1.retornaLevel());
+		System.out.println("Hash do cavaleiro: " + cavaleiro1.hashCode());
 		System.out.println("A arma do cavaleiro " + cavaleiro1.retornaNome() + " eh a " + cavaleiro1.retornaArma().retornaNome());
 		System.out.println("Descricao da arma do cavaleiro " + cavaleiro1.retornaNome());
 		System.out.println("Nome : " + cavaleiro1.retornaArma().retornaNome());
@@ -15,13 +16,14 @@ public class Main {
 		System.out.println("Durabilidade : " + cavaleiro1.retornaArma().retornaDurabilidade());
 		System.out.println("Hash da arma: " + cavaleiro1.retornaArma().hashCode() + "\n");
 		System.out.println("Cavaleiro " + cavaleiro2.retornaNome() + " de level " + cavaleiro2.retornaLevel());
+		System.out.println("Hash do cavaleiro: " + cavaleiro2.hashCode());
 		System.out.println("A arma do cavaleiro " + cavaleiro2.retornaNome() + " eh a " + cavaleiro2.retornaArma().retornaNome());
 		System.out.println("Descricao da arma do cavaleiro " + cavaleiro2.retornaNome());
 		System.out.println("Nome : " + cavaleiro2.retornaArma().retornaNome());
 		System.out.println("Dano : " + cavaleiro2.retornaArma().retornaDano());
 		System.out.println("Durabilidade : " + cavaleiro2.retornaArma().retornaDurabilidade());
 		System.out.println("Hash da arma: " + cavaleiro2.retornaArma().hashCode() + "\n");
-		System.out.println("\n=================================================================================\n");
+		System.out.println("=================================================================================\n");
 		
 	}
 
